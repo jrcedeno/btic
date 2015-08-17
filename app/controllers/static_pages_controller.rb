3ffe2a600beec9331a9147bb12ed19632fbf5072
Julilan Rodriguez <julian@sonetica.net>
2015-08-17 15:47:39 -0400

    correct static pages spelling error

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
-class StaicPagesController < ApplicationController
+class StaticPagesController < ApplicationController
+  
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
 
-  get 'home'      =>  'staic_pages#home'
-  get 'about'     =>  'staic_pages#about'
-  get 'partners'  =>  'staic_pages#partners'
-  get 'media'     =>  'staic_pages#media'
-  get 'contact'   =>  'staic_pages#contact'
+  get 'home'      =>  'static_pages#home'
+  get 'about'     =>  'static_pages#about'
+  get 'partners'  =>  'static_pages#partners'
+  get 'media'     =>  'static_pages#media'
+  get 'contact'   =>  'static_pages#contact'
 
 end
